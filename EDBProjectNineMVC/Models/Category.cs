c754bd9fa26d0e734d2dbd497e5d6f65ab3546d3
edanielbluford <e.daniel.bluford@gmail.com>
2018-07-13 13:04:19 -0400

    first commit

---
+++ EDBProjectNineMVC/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EDBProjectNineMVC.Models
+{
+    public class Category
+    {
+        
+        public int CategoryID { get; set; }
+
+        [DisplayName("Category")]
+        public string CategoryType { get; set; }
+
+        public virtual ICollection<Review> Reviews { get; set; }
+    }
+
+}
---
+++ EDBProjectNineMVC/Models/Review.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EDBProjectNineMVC.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewID { get; set; }
+
+        private DateTime reviewdate;
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("Review")]
+        public string Content { get; set; }
+
+        [DisplayName("Review Date")]
+        public DateTime ReviewDate
+        {
+            get { return this.reviewdate; }
+            set { this.reviewdate = DateTime.Now; }
+        }
+
+
+        [ForeignKey("Category")]
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
+
+
+
+    }
+}
